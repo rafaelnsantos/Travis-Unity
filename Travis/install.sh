aaac002c8e09e96430540aca45cfa1e6e4616fb3
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-23 16:25:50 -0300

    cache unity downloads Folder

--- Travis/install.sh
+++ Travis/install.sh
@@ -7,20 +7,31 @@ HASH=a9f86dcd79df
 VERSION=2017.3.0f3
 
 download() {
-  file=$1
-  url="$BASE_URL/$HASH/$package"
+ file=$1
+ url="$BASE_URL/$HASH/$package"
 
-  echo "Downloading from $url: "
-  curl -o `basename "$package"` "$url"
+ echo "Downloading from $url: "
+ cd Unity
+ curl -o `basename "$package"` "$url"
+ cd ../
 }
 
 install() {
-  package=$1
-  download "$package"
+ package=$1
+ filename=`basename "$package"`
+ packagePath="Unity/$filename"
+ if [ ! -f $packagePath ] ; then
+   echo "$packagePath not found. downloading `basename "$packagePath"`"
+   download "$package"
+ fi
 
-  echo "Installing "`basename "$package"`
-  sudo installer -dumplog -package `basename "$package"` -target /
+ echo "Installing "`basename "$package"`
+ sudo installer -dumplog -package $packagePath -target /
 }
+
+if [ ! -d "Unity" ] ; then
+ mkdir -p -m 777 Unity
+fi
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
