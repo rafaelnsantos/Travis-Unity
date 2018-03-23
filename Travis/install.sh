117a614a5571eb8c9197b40a593d3b4b82ac66d5
Rafael Santos <rafaelnsantos1@gmail.com>
2018-03-23 16:19:07 -0300

    mac now default

--- Travis/install.sh
+++ Travis/install.sh
@@ -24,6 +24,5 @@ install() {
 
 install "MacEditorInstaller/Unity-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
 install "MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-$VERSION.pkg"
