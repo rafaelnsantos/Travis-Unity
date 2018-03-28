16a640cc03a43fe9870294e43bcdd09765210c5b
Arthur Aguiar <arthur94_br@hotmail.com>
2018-03-28 15:40:42 -0300

    Importei um minino que corre

---
+++ Assets/_Projeto/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	Animator anim;
+	float direcao;
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			anim.SetInteger("Direcao", (int)Input.GetAxisRaw("Horizontal"));
+	}
+}
